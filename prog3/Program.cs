ac952e014ae47f12daea5390b1adbd4e114dc376
Anatoly <up.many.money@gmail.com>
2022-09-08 01:32:48 +0300

    Modernized prog3

--- prog3/Program.cs
+++ prog3/Program.cs
@@ -10,16 +10,23 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter three number:");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-int res = 0;
-if (num1 + num2 > num3) res = 1;
-if (num3 + num1 > num2) res = 1;
-if (num2 + num3 > num1) res = 1;
+// int res = 0;
+// if (num1 + num2 > num3) res = 1;
+// if (num3 + num1 > num2) res = 1;
+// if (num2 + num3 > num1) res = 1;
 
-if (num1 + num2 < num3) res = 0;
-if (num3 + num1 < num2) res = 0;
-if (num2 + num3 < num1) res = 0;
+// if (num1 + num2 < num3) res = 0;
+// if (num3 + num1 < num2) res = 0;
+// if (num2 + num3 < num1) res = 0;
 
-if (res == 0) Console.WriteLine("Triangle No");
-else Console.WriteLine("Triangle Yes");
+// if (res == 0) Console.WriteLine("Triangle No");
+// else Console.WriteLine("Triangle Yes");
 
 // Сделал самостоятельно, знаю, код не технологичен (Много if).
+// У группы подсмотрел более оптимальное решение.
+
+if (num1 + num2 > num3 & num3 + num1 > num2 & num2 + num3 > num1)
+{
+    Console.WriteLine("Triangle Yes");
+}
+else Console.WriteLine("Triangle No");
